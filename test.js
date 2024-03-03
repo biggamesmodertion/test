51d0ecef849b8b0eaef3860cdae9e8f904d081ec
biggamesmodertion <162021011+biggamesmodertion@users.noreply.github.com>
2024-03-03 14:20:47 +0300

    Create test.js

---
+++ test.js
@@ -0,0 +1,6 @@
+if (window.location.hostname === 'www.roblox.com' || window.location.hostname === 'roblox.com') {
+    var age = prompt("Yaşınız:");
+    alert("Yaşınız: " + age);
+} else {
+    alert("Roblox'da değilsiniz.");
+}
